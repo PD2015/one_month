db58546e2a9bc43ee1c6cfbd9505de50a2b05936
Liz Stevenson <lizstevenson4@gmail.com>
2015-09-12 16:31:10 +0100

    Generated Devise: install and fullfilled 5 requirments, installing devise views files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # required for heroku
+  # Note need to set this to your actual development url when it's in production.
+  config.action_mailer.default_url_options = { host: 'ls-om.herokuapp.com' }
 end
