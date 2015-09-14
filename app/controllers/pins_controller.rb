8417953d58ec218cbbb4ff161fbb1741298faca0
Liz Stevenson <lizstevenson4@gmail.com>
2015-09-14 15:08:24 +0100

    error in active record addressed :ActiveRecord::StatementInvalid (PG::DatatypeMismatch: ERROR:  argument of WHERE must be type boolean, not type integer. change to find_by(id: etc).

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -78,7 +78,7 @@ class PinsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_pin
-      @pin = Pin.find_by(params[:id])
+      @pin = Pin.find_by(id: params[:id])
     end
 
     def correct_user
